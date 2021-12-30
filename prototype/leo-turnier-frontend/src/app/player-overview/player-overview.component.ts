cb1c98aba48110da610b7849e04f3f4f8e9b91ce
BenjaminEcker <benjamin.ecker1604@gmail.com>
2021-12-30 11:58:40 +0100

    corrected the style and the name cols of the list

--- prototype/leo-turnier-frontend/src/app/player-overview/player-overview.component.ts
+++ prototype/leo-turnier-frontend/src/app/player-overview/player-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {ApiService} from '../services/api.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-player-overview',
@@ -9,13 +10,24 @@ import {ApiService} from '../services/api.service';
 })
 export class PlayerOverviewComponent implements OnInit {
   dataSource = new MatTableDataSource();
-  displayedColumns: string[] = ['id', 'name', 'totalScore', 'birthdate', 'team'];
+  displayedColumns: string[] = ['id', 'name', 'totalScore', 'birthdate', 'team', 'actions'];
 
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, public router: Router) { }
 
   async ngOnInit(): Promise<void> {
     this.dataSource.data = await this.api.getPlayers();
   }
 
+   async deletePlayer(id: number): Promise<void> {
+     if (confirm('Are you sure to delete this record ?')) {
+       await this.api.deletePlayer(id);
+       await new Promise(f => setTimeout(f, 10));
+     }
+     window.location.reload();
+   }
+
+  editPlayer(id: number): void {
+    this.router.navigate(['player-detail'], {state: {data: id}});
+  }
 }
