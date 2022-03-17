04c277c27cf768e635852f0dd2c2bdcbbcc67b72
LukasHain <lukashain9@gmail.com>
2022-03-17 08:28:00 +0100

    removed Create, Update and Delete endpoints from TournamentModeService #tourn-12

--- prototype/leo-turnier-backend/src/main/java/at/htl/LeoTurnier/boundary/TournamentModeService.java
+++ prototype/leo-turnier-backend/src/main/java/at/htl/LeoTurnier/boundary/TournamentModeService.java
@@ -18,32 +18,6 @@ public class TournamentModeService {
     @Inject
     TournamentModeRepository repository;
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response add(TournamentMode tournamentMode, @Context UriInfo info) {
-        tournamentMode = repository.add(tournamentMode);
-        if (tournamentMode == null) {
-            return Response.status(204).build();
-        }
-        return Response.created(info
-                .getAbsolutePathBuilder()
-                .queryParam("id", tournamentMode.getId()).build()).build();
-    }
-
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response modify(@QueryParam("id") long id, TournamentMode tournamentMode, @Context UriInfo info) {
-        tournamentMode = repository.modify(id, tournamentMode);
-        if (tournamentMode == null) {
-            return Response.status(204).build();
-        }
-        return Response.created(info
-                .getAbsolutePathBuilder()
-                .queryParam("id", tournamentMode.getId()).build()).build();
-    }
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response get(@QueryParam("id") Long id) {
@@ -51,11 +25,5 @@ public class TournamentModeService {
             return Response.ok(repository.getById(id)).build();
         }
         return Response.ok(repository.getAll()).build();
-    }
-
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response delete(@QueryParam("id") long id) {
-        return Response.ok(repository.delete(id)).build();
     }
 }
