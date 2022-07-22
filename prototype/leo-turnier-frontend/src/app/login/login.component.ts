2f96b24e48a072f0e5c51ede358bde211bc97f4b
BenjaminEcker <benjamin.ecker1604@gmail.com>
2022-07-22 14:07:50 +0200

    finished model for login

--- prototype/leo-turnier-frontend/src/app/login/login.component.ts
+++ prototype/leo-turnier-frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginValid = true;
+  public username = '';
+  public password = '';
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
   }
 
+  public ngOnInit(): void {
+  }
+
+  public onSubmit(): void {
+    this.loginValid = true;
+  }
 }
