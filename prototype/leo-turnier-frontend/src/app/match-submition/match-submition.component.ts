a802719dc4c07f6fb6061a821ddff52668356956
BenjaminEcker <benjamin.ecker1604@gmail.com>
2022-08-13 11:47:13 +0200

    submit works but only with refreshing the page

--- prototype/leo-turnier-frontend/src/app/match-submition/match-submition.component.ts
+++ prototype/leo-turnier-frontend/src/app/match-submition/match-submition.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {MatchService} from '../services/match.service';
+import {Match} from '../model/match.model';
+import {Competitor} from '../model/competitor.model';
+import {map} from 'rxjs/operators';
+import {Location} from '@angular/common';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-match-submition',
@@ -6,10 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./match-submition.component.scss']
 })
 export class MatchSubmitionComponent implements OnInit {
+  id = '';
+  match = new Match(0, new Competitor(0, 'comp 1'), new Competitor(0, 'comp 2'), 0, 0, false);
 
-  constructor() { }
+  submitionForm = new FormGroup({
+    score1: new FormControl('', [Validators.required]),
+    score2: new FormControl('', [Validators.required]),
+  });
 
-  ngOnInit(): void {
+  constructor(private activatedRouter: ActivatedRoute,
+              private location: Location,
+              private api: MatchService) {
+    // @ts-ignore
+    // tslint:disable-next-line:no-shadowed-variable
+    activatedRouter.paramMap.subscribe(map => this.id = map.get('id'));
   }
 
+  async ngOnInit(): Promise<void> {
+    this.match = await this.api.getMatch(this.id);
+    this.submitionForm.setValue({score1: this.match.score1, score2: this.match.score2});
+    console.log(this.match);
+    console.log(this.submitionForm.value);
+  }
+
+  async onSubmit(): Promise<void> {
+    this.match.score1 = this.submitionForm.value.score1;
+    this.match.score2 = this.submitionForm.value.score2;
+
+    // @ts-ignore
+    const action = document.activeElement.getAttribute('Name');
+    console.log(action);
+
+    await this.api.updateMatch(this.match);
+
+    this.location.back();
+  }
 }
--- prototype/leo-turnier-frontend/src/app/services/match.service.ts
+++ prototype/leo-turnier-frontend/src/app/services/match.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Match} from '../model/match.model';
+import {KeycloakService} from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,27 @@ import {Match} from '../model/match.model';
 export class MatchService {
   private host = 'http://localhost:8080/api/match';
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              private keycloakService: KeycloakService) {
   }
 
   async getMatches(id: string): Promise<Match[]>{
     return await this.httpClient.get<Match[]>(this.host + '?tournamentId=' + id).toPromise();
+  }
+
+  async getMatch(id: string): Promise<Match> {
+    return await this.httpClient.get<Match>(this.host + '?id=' + id).toPromise();
+  }
+
+  async updateMatch(match: Match): Promise<void> {
+    const authToken = this.keycloakService.getToken();
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authToken}`
+    };
+    await this.httpClient.put(this.host + '?id=' + match.id, JSON.stringify(match), {headers}).subscribe(
+      data => console.log('success', data),
+      error => console.log('oops', error)
+    );
   }
 }
