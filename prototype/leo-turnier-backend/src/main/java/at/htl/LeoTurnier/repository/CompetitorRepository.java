e7f19cfb3c9f6de2e36f5675fe5b76c265d9db1d
LukasHain <lukashain9@gmail.com>
2022-08-20 11:40:26 +0200

    test workflow

--- prototype/leo-turnier-backend/src/main/java/at/htl/LeoTurnier/repository/CompetitorRepository.java
+++ prototype/leo-turnier-backend/src/main/java/at/htl/LeoTurnier/repository/CompetitorRepository.java
@@ -54,7 +54,7 @@ public class CompetitorRepository implements PanacheRepository<Competitor> {
                 "select pt.competitor " +
                         "from Participation pt " +
                         "where pt.tournament.id = :tournamentId " +
-                        "order by pt.placement desc, pt.seed desc", Competitor.class);
+                        "order by pt.placement, pt.seed", Competitor.class);
         getById.setParameter("tournamentId", tournamentId);
         return getById.getResultList();
     }
